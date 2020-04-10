6110dc5cbc509192e68f5407524e82905a5546db
TheVTomi <viczinat@gmail.com>
2020-04-10 12:22:25 +0200

    összeolvasztott

---
+++ Beadandó1/templates/pages/araink.tpl.php
@@ -0,0 +1,57 @@
+<table>
+    <tr>
+        <th>Gyors gépi mosás</th>
+        <th>Normál Gépi mosás</th>
+        <th>Kézi mosás (alkalmazott által)</th>
+        <th>Kézi mosás (vásárló által)</th>
+    </tr>
+    <tr>
+        <td>Gépi habos mosás</td>
+        <td>Gépi habos mosás</td>
+        <td>Kézi habos mosás</td>
+        <td>Kézi habos mosás</td>
+    </tr>
+    <tr>
+        <td>Öblítés</td>
+        <td>Öblítés</td>
+        <td>Kézi törlés & öblítés</td>
+        <td>5 perc bogároldó</td>
+    </tr>
+    <tr>
+        <td>Szárítás</td>
+        <td>WAX</td>
+        <td>Kerekek kézi tisztítása</td>
+        <td>10 perc kefés mosás</td>
+    </tr>
+    <tr>
+        <td>-</td>
+        <td>Szárítás</td>
+        <td>Kézi WAX</td>
+        <td>5 perc forróvizes mosás</td>
+    </tr>
+    <tr>
+        <td>-</td>
+        <td>-</td>
+        <td>Cseppmentes szárítás</td>
+        <td>5perc WAX</td>
+    </tr>
+	<tr>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+    </tr>
+    <tr>
+        <td>10perc</td>
+        <td>15perc</td>
+        <td>25perc</td>
+        <td>25perc</td>
+    </tr>
+
+    <tr>
+        <th>2000Ft</th>
+        <th>3000Ft</th>
+        <th>5000Ft</th>
+        <th>4000Ft</th>
+    </tr>
+</table>
---
+++ Beadandó1/templates/pages/kapcsolat.tpl.php
@@ -0,0 +1,8 @@
+<div id=kapcsolat>
+<h2>Elérhetőségeink:</h2>
+<p>Telefonszám <strong>555-5555</strong></p>
+<p>E-mail: <strong>info@tisztaverda.hu</strong></p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9171.78434313157!2d19.65702739195656!3d46.887223033854866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9fbac53fe703386c!2zQXV0w7Ntb3PDsyBLw6l6aSBBdXTDs21vc8OzIMOpcyBhdXTDs2tvem1ldGlrYSBBdXTDs8OhcG9sw6Fz!5e0!3m2!1shu!2shu!4v1586513986611!5m2!1shu!2shu" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" allowfullscreen ></iframe>
+<br>
+<a target="_blank" href="">Nagyobb térkép</a>
+</div id=kapcsolat>
